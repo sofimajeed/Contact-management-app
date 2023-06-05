ea70f18799426d89c16a4291056f40c0848b0358
Sofi Majeed <majeed.sofi@jungleworks.com>
2023-06-06 00:02:47 +0530

    First Commit

---
+++ src/App.tsx
@@ -0,0 +1,31 @@
+
+import { useLocation } from "react-router-dom";
+import './App.css';
+import SideBar from './Components/SideBar';
+import AllRoutes from './Pages/AllRoutes';
+
+function App() {
+
+  const location = useLocation();
+  const currentRoute = location.pathname;
+ 
+  return (
+    <div className="App">
+      <h1 className='z-50 corona-title w-full fixed shadow-sm shadow-slate-700 top-0 text-2xl text-yellow-100 bg-indigo-300 font-bold p-4'>
+        {
+          currentRoute == "/" || currentRoute == "/contact_form"? ' Contact Management App' : "Corona Through Maps"
+        }
+      </h1>
+      <div className='flex w-full responsive '>
+        <div className='sticky  top-0 h-screen'>
+          <SideBar />
+        </div>
+        <div className='w-full'>
+          <AllRoutes />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
